b9c3f04ce5d623007beb578c77753d3ee1ee2796
shomil03 <shomil02020@gmail.com>
2024-12-27 21:41:21 +0530

    bug fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
                     // break;
 
                 case "echo":
-                    System.out.println();
+                    // System.out.println();
                     for(int i = 1 ; i < input.length ; i++) {
                         System.out.print(input[i] +" ");
                     }
