6bacc1d7feb102f531ca5c9149e638b8fe56c31d
shomil03 <shomil02020@gmail.com>
2024-12-27 21:22:21 +0530

    bug ficed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
         System.out.print("$ "); 
 
         Scanner scanner = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
         while(scanner.hasNext()){
-            String input = scanner.nextLine();
+            String input = scanner.next();
+            // inputs.add(scanner.nextLine())
             System.out.println(input+": command not found");
         }
     }
