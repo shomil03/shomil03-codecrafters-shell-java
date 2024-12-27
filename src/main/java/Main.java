c56e21fb160aef2b5b42fa8b34baf7ab93ad7b32
shomil03 <shomil02020@gmail.com>
2024-12-27 21:20:00 +0530

    implemented relp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,9 @@ public class Main {
         System.out.print("$ "); 
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
-        System.out.println(input+": command not found");
+        while(scanner.hasNext()){
+            String input = scanner.nextLine();
+            System.out.println(input+": command not found");
+        }
     }
 }
